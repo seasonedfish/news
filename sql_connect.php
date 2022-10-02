6cedab0bb8f64bf9a579aaea23f9b5f82dbc52f7
Fisher Sun <fisher521.fs@gmail.com>
2022-10-01 20:59:44 -0500

    Fill in code for sql_connect

--- sql_connect.php
+++ sql_connect.php
@@ -2,3 +2,10 @@
 /**
  * The code for connecting into the SQL database, can be required by other files.
  */
+
+$mysqli = new mysqli("localhost", "news_inst", "new_pass", "news");
+
+if ($mysqli->connect_errno) {
+    printf("Connection Failed: %s\n", $mysqli->connect_error);
+    exit;
+}
