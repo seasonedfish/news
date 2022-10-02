fa692a3492e3e696601d1ff74c43e9d12b8392fd
Fisher Sun <f.sun@wustl.edu>
2022-10-02 16:32:16 -0500

    Add code and css for header

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,10 @@
 <header>
-    <h1>News site</h1>
-    <p></p>
+    <div id="header-title">
+        <b>News</b>
+    </div>
+    <div id="header-right">
+        hi
+    </div>
+    <div id="left">  left  </div>
+    <div id="right"> right </div>
 </header>
