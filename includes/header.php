19a4fa4c8bece6fd92362d6fd2b699fc09e3176c
Fisher Sun <f.sun@wustl.edu>
2022-10-02 18:36:43 -0500

    Check if user is signed in for header

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,18 @@
     <nav>
         <a href="index.php"><b>News</b></a>
         <span class="nav-right">
-            <a href="submit_post.php">Submit post</a> 
-            <a href="profile.php">Profile</a>
+            <?php
+                if (isset($_SESSION['username'])) {
+                    echo <<<EOF
+                    <a href="submit_post.php">Submit post</a> 
+                    <a href="profile.php">Profile</a>
+                    EOF;
+                } else {
+                    echo <<<EOF
+                    <a href="sign_in.php">Sign in/register</a> 
+                    EOF;
+                }
+            ?>
         </span>
     </nav>
 </header>
