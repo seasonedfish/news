122abef390461bf8074fd4e1bad204258b8bdfee
Fisher Sun <f.sun@wustl.edu>
2022-10-02 18:49:07 -0500

    Add basic profile page

--- profile.php
+++ profile.php
@@ -2,3 +2,27 @@
 /**
  * Displays a user's profile.
  */
+
+ function main() {
+    session_start();
+    $_SESSION["username"] = "fisher"; // DEBUG
+ }
+
+ main();
+
+ include "includes/head.php";
+ ?>
+
+ <body>
+    <?php
+    include "includes/header.php";
+    ?>
+
+    <main>
+        <h1>Profile</h1>
+    </main>
+</body>
+
+<?php
+include  "includes/tail.php";
+?>
