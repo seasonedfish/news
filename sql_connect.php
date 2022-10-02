951db80f45fa9302b6d8d4ba0017e18fb36b80a7
Fisher Sun <fisher521.fs@gmail.com>
2022-10-01 21:30:40 -0500

    Require sql_connect.php

--- sql_connect.php
+++ sql_connect.php
@@ -3,9 +3,13 @@
  * The code for connecting into the SQL database, can be required by other files.
  */
 
-$mysqli = new mysqli("localhost", "news_inst", "new_pass", "news");
+function get_mysqli() {
+    $mysqli = new mysqli("localhost", "news_inst", "new_pass", "news");
 
-if ($mysqli->connect_errno) {
-    printf("Connection Failed: %s\n", $mysqli->connect_error);
-    exit;
+    if ($mysqli->connect_errno) {
+        printf("Connection Failed: %s\n", $mysqli->connect_error);
+        exit;
+    }
+
+    return $mysqli;
 }
--- sql_queries.php
+++ sql_queries.php
@@ -2,3 +2,7 @@
 /**
  * Shared functions for SQL queries.
  */
+
+require_once "sql_connect.php";
+
+$mysqli = get_mysqli();
