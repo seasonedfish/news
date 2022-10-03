32835acdf7d2f99be5692d8a1f528775d631c8f7
Fisher Sun <fisher521.fs@gmail.com>
2022-10-03 04:01:45 -0500

    Update head title

--- includes/head.php
+++ includes/head.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>File Sharing</title>
+    <title>News</title>
 
     <?php
     # This path is in the perspective of the file that includes, so it should not have ../
     ?>
+    <!--suppress HtmlUnknownTarget -->
     <link rel=stylesheet href="assets/css/main.css">
 </head>
