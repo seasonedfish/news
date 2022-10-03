1be4092c0c612398c268d4ecd239e3e79705f2d3
Fisher Sun <f.sun@wustl.edu>
2022-10-02 21:13:33 -0500

    add post not found

---
+++ post_not_found.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The server cannot find a given post.
+ */
+
+ include "includes/head.php";
+ ?>
+
+ <body>
+    <?php
+    include "includes/header.php";
+    ?>
+
+    <main>
+        <h1>
+            Post not found
+        </h1>
+        <p>
+            The requested post doesn't seem to exist.
+        </p>
+    </main>
+</body>
+
+<?php
+include  "includes/tail.php";
+?>
