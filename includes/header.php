271ae7b8ee5cac601927e0e7a34b3a0252596b65
Fisher Sun <fisher521.fs@gmail.com>
2022-10-03 03:16:43 -0500

    Fix profile link

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,17 @@
 <header>
     <nav>
+        <!--suppress HtmlUnknownTarget -->
         <a href="index.php"><b>News</b></a>
         <span class="nav-right">
             <?php
                 if (isset($_SESSION['username'])) {
                     echo <<<EOF
                     <a href="submit_post.php">Submit post</a> 
-                    <a href="profile.php">Profile</a>
+                    <a href="profile.php?username={$_SESSION['username']}">Profile</a>
                     <a href="sign_out.php">Sign out</a>
                     EOF;
                 } else {
+                    /** @noinspection HtmlUnknownTarget */
                     echo <<<EOF
                     <a href="sign_in.php">Sign in/register</a> 
                     EOF;
