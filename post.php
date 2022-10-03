ee8d618458bb9278b77e315aa8f10b730042e6ae
Fisher Sun <f.sun@wustl.edu>
2022-10-02 21:13:58 -0500

    fill in post

--- post.php
+++ post.php
@@ -1,2 +1,22 @@
 <?php
-// Displays a post and its comments.
+/**
+ * Displays a post and its comments.
+ */
+require_once "sql_queries.php";
+// TODO: PROTECT AGAINST SQL INJECTION
+$post = get_post($GET["post_id"]);
+
+if (empty($post)) {
+    header("Location: post_not_found.php");
+    exit();
+}
+
+include "includes/head.php";
+?>
+
+<body>
+    <?php include "includes/header.php" ?>
+
+    <main>
+    </main>
+</body>
