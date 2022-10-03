226d0baf062dfe3ee5d0df9d86b6f83dbddbb81e
Fisher Sun <f.sun@wustl.edu>
2022-10-02 20:35:01 -0500

    Add comments to sql_queries

--- sql_queries.php
+++ sql_queries.php
@@ -9,6 +9,9 @@ $mysqli = get_mysqli();
 
 
 function fetch_query(string $query) {
+    /**
+     * Fetches a query from the database.
+     */
     global $mysqli;
 
     $result = $mysqli->query($query);
@@ -16,13 +19,23 @@ function fetch_query(string $query) {
 }
 
 function get_all_posts() {
+    /**
+     * Returns an array of associative arrays (each associative array contains one post's info).
+     */
     return fetch_query("SELECT * FROM posts");
 }
 
 function get_post_comments(int $post_id) {
+    /**
+     * Returns an array of associative arrays (each associative array contains one comment's info).
+     */
     return fetch_query("SELECT * FROM comments WHERE post_id = " . $post_id);
 }
 
 function get_user(string $username) {
+    /**
+     * Returns an associative array representing a single user.
+     */
+    // fetch_query returns an array of rows, but we only want a single row, so we return the first row.
     return fetch_query("SELECT * FROM users WHERE username = '" . $username . "'")[0];
 }
