ceb9f0bc37368f3249dd2553b956804bc49916af
harrygao56 <harrygao56@gmail.com>
2022-10-03 07:10:55 -0500

    changes to index

--- index.php
+++ index.php
@@ -15,7 +15,10 @@
         <?php 
         $rows = get_all_posts();
         foreach ($rows as $row) {
-            printf("%s (%s) \n", $row["title"], $row["username"]);
+            printf("<div>
+                <a href=\"post.php?post_id=%u\">%s</a> --- posted by %s <br>
+                <a href=\"%s\">%s</a>
+            </div>", $row["post_id"], $row["title"], $row["username"], $row["link"], $row["link"]);
         }
         ?>
         </p>
--- post.php
+++ post.php
@@ -52,7 +52,7 @@ include "includes/head.php";
         <small>
             Submitted by
             <a href=<?php echo "profile.php?username=" . $author["username"] ?>>
-                <?php echo $author["username"]; ?>
+                <?php echo $author["username"];?>
             </a>
             on <?php echo date_format(date_create($post["post_date"]), "l, F jS, Y"); ?>
         </small>
