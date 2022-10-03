bd9e52015da9e24782f116e48e2542a5b1250dae
Fisher Sun <fisher521.fs@gmail.com>
2022-10-03 02:15:08 -0500

    Remove echo

--- post.php
+++ post.php
@@ -73,7 +73,7 @@ include "includes/head.php";
         <?php
         if (isset($_SESSION["username"])) {
             echo <<<EOF
-            <form action="<?php echo {$_SERVER['PHP_SELF']}; ?>" method="POST">
+            <form action="{$_SERVER['PHP_SELF']}" method="POST">
                 <p>
                     <textarea id="comment" name="comment" rows="4" cols="50"></textarea>
                 </p>
